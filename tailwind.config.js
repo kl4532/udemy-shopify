26944b58af3cf0d1f84be721030d8097c44693a2
klukasiak <kl@webundsoehne.com>
2022-08-05 22:02:59 +0200

    * init Elizabeth * init tailwind * added gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./**/*.{liquid,json}"
+      // "./assets/*.liquid",
+      // "./layout/*.liquid",
+      // "./sections/*.liquid",
+      // "./snippets/*.liquid",
+      // "./templates/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
